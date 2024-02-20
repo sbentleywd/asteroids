eb4a0461da5234c23f646377798205a587ee36ac
Simon Bentley <simon.bentley@corestream.co.uk>
2024-02-20 16:42:23 +0000

    Math class

--- classes/mass.js
+++ classes/mass.js
@@ -1,17 +1,29 @@
+const gameWidth = 600
+const gameHeight = 400
+
 class Mass {
-  constructor(x, y, mass, radius, angle, x_speed, y_speed, rotation_speed) {
-    this.x = x
-    this.y = y
-    this.mass = mass || 1
-    this.radius = radius || 50
-    this.angle = angle || 0
-    this.x_speed = x_speed || 0
-    this.y_speed = y_speed || 0
-    this.rotation_speed = rotation_speed || 0
+  constructor(x, y, mass, radius, rotationAngle, xSpeed, ySpeed, rotationSpeed) {
+    this.x = x;
+    this.y = y;
+    this.mass = mass || 1;
+    this.radius = radius || 50;
+    this.rotationAngle = rotationAngle || 0;
+    this.xSpeed = xSpeed || 0;
+    this.ySpeed = ySpeed || 0;
+    this.rotationSpeed = rotationSpeed || 0;
   }
 
-  update() {
-    this.x += this.x_speed
-    this.y += this.y_speed
+  update(elapsed) {
+    this.x += this.xSpeed * elapsed;
+    this.y += this.ySpeed * elapsed;
+    this.rotationAngle += this.rotationSpeed * elapsed;
+
+    if (this.x - this.radius > gameWidth) this.x = -this.radius
+    if (this.x + this.radius < 0) this.x = gameWidth + this.radius
+    if (this.y - this.radius > gameHeight) this.y = -this.radius
+    if (this.y + this.radius < 0) this.y = gameHeight + this.radius
+
   }
 }
+
+export { Mass };
--- main.js
+++ main.js
@@ -1,38 +1,41 @@
-import { Asteroid } from './classes/asteroid.js'
+import { Asteroid } from "./classes/asteroid.js";
 
-const asteroid1 = new Asteroid(9, 200, 200, 100, 1)
+const asteroid1 = new Asteroid(300, 200, 1, 20, 0, 10, -100, 50, 12, {});
+const asteroid2 = new Asteroid(300, 200, 1, 20, 0, -20, 50, 50, 16, {});
 
-let previous, elapsed
+let previous, elapsed;
 
 const draw = () => {
-  let svgString = initSVG()
+  let svgString = initSVG();
 
-  svgString += asteroid1.draw()
+  svgString += asteroid1.draw();
+  svgString += asteroid2.draw();
 
-  svgString += closeSVG()
+  svgString += closeSVG();
 
-  document.getElementById('asteroids').innerHTML = svgString
-}
+  document.getElementById("asteroids").innerHTML = svgString;
+};
 
-const upDate = () => {
-  asteroid1.upDate()
-}
+const update = () => {
+  asteroid1.update(elapsed / 1000);
+  asteroid2.update(elapsed / 1000);
+};
 
 const frame = (timeStamp) => {
-  if (!previous) previous = timeStamp
-  elapsed = previous - timeStamp
-  draw()
-  upDate()
-  previous = timeStamp
-  requestAnimationFrame(frame)
-}
+  if (!previous) previous = timeStamp;
+  elapsed = timeStamp - previous;
+  draw();
+  update();
+  previous = timeStamp;
+  requestAnimationFrame(frame);
+};
 
-requestAnimationFrame(frame)
+requestAnimationFrame(frame);
 
 const initSVG = () => {
-  return `<svg width="400" height="400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 400" id="asteroidsSVG" >`
-}
+  return `<svg width="600" height="400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 600 400" id="asteroidsSVG" >`;
+};
 
 const closeSVG = () => {
-  return `</svg>`
-}
+  return `</svg>`;
+};
